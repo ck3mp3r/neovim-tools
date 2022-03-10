ab6d9236140afc144995ec4dd86c6aa4dbc1af1b
Christian Kemper <christian.kemper@pm.me>
2022-03-10 12:03:20 +0000

    remapping debug

--- lua/core/dap/keymaps.lua
+++ lua/core/dap/keymaps.lua
@@ -2,23 +2,18 @@ local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- debugging
-keymap("n", "<F4>", ":lua require('dapui').toggle()<CR>", opts)
-keymap("n", "<F5>", ":lua require('dap').toggle_breakpoint()<CR>", opts)
-keymap("n", "<F9>", ":lua require('dap').continue()<CR>", opts)
+keymap("n", "<A-d>", ":lua require('dapui').toggle()<CR>", opts)
+keymap("n", "<A-b>", ":lua require('dap').toggle_breakpoint()<CR>", opts)
+keymap("n", "<A-c>", ":lua require('dap').continue()<CR>", opts)
 
-keymap("n", "<F1>", ":lua require('dap').step_over()<CR>", opts)
-keymap("n", "<F2>", ":lua require('dap').step_into()<CR>", opts)
-keymap("n", "<F3>", ":lua require('dap').step_out()<CR>", opts)
+keymap("n", "<A-o>", ":lua require('dap').step_over()<CR>", opts)
+keymap("n", "<A-i>", ":lua require('dap').step_into()<CR>", opts)
+keymap("n", "<A-u>", ":lua require('dap').step_out()<CR>", opts)
 
-keymap("n", "<Leader>dsc", ":lua require('dap').continue()<CR>", opts)
-keymap("n", "<Leader>dsv", ":lua require('dap').step_over()<CR>", opts)
-keymap("n", "<Leader>dsi", ":lua require('dap').step_into()<CR>", opts)
-keymap("n", "<Leader>dso", ":lua require('dap').step_out()<CR>", opts)
+keymap("n", "<A-v>h", ":lua require('dap.ui.variables').hover()<CR>", opts)
+keymap("v", "<A-v>v", ":lua require('dap.ui.variables').visual_hover()<CR>", opts)
 
-keymap("n", "<Leader>dhh", ":lua require('dap.ui.variables').hover()<CR>", opts)
-keymap("v", "<Leader>dhv", ":lua require('dap.ui.variables').visual_hover()<CR>", opts)
-
-keymap("n", "<Leader>duh", ":lua require('dap.ui.widgets').hover()<CR>", opts)
+keymap("n", "<A-v>w", ":lua require('dap.ui.widgets').hover()<CR>", opts)
 keymap(
   "n",
   "<Leader>duf",
@@ -36,7 +31,6 @@ keymap(
   ":lua require('dap').set_breakpoint({ nil, nil, vim.fn.input('Log point message: ') )<CR>",
   opts
 )
-keymap("n", "<Leader>dbt", ":lua require('dap').toggle_breakpoint()<CR>", opts)
 
 keymap("n", "<Leader>dc", ":lua require('dap.ui.variables').scopes()<CR>", opts)
 keymap("n", "<Leader>di", ":lua require('dapui').toggle()<CR>", opts)
