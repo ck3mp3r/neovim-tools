72b035dbf86cd0fb05481ec0a11aee5971ec8e83
Christian Kemper <christian.kemper@pm.me>
2022-03-31 16:48:06 +0100

    adding helm chart support... not for templates though

--- lua/core/lsp/settings/yamlls.lua
+++ lua/core/lsp/settings/yamlls.lua
@@ -2,7 +2,8 @@ local opts = {
   settings = {
     yaml = {
       schemas = {
-        ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*"
+        ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+        ["https://json.schemastore.org/chart.json"] = "*/templates/**.yaml"
       },
     },
   },
